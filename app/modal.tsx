4df72508b4623fb35cee72cff3194f6682ef837d
Austen Belmonte <26863753+KiwiLemons@users.noreply.github.com>
2024-02-26 13:36:40 -0600

    Try to upload to the backend

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,49 +1,77 @@
 import { StatusBar } from 'expo-status-bar';
-import { Platform, StyleSheet, Button } from 'react-native';
+import { Platform, StyleSheet, Button, Switch } from 'react-native';
 import React, { useState, useEffect } from 'react';
-import { Text, View} from '../components/Themed';
+import { Text, View } from '../components/Themed';
 import * as TaskManager from 'expo-task-manager';
 import * as Location from 'expo-location';
+import Colors from '../constants/Colors';
 
 const LOCATION_TASK_NAME = 'background-location-task';
+const BACKEND_URL = process.env.EXPO_PUBLIC_BACKEND_URL;
 
-const requestPermissions = async () => {
-  const { status: foregroundStatus } = await Location.requestForegroundPermissionsAsync();
-  if (foregroundStatus === 'granted') {
-    const { status: backgroundStatus } = await Location.requestBackgroundPermissionsAsync();
-    if (backgroundStatus === 'granted') {
-      await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
-        accuracy: Location.Accuracy.Highest,
-        deferredUpdatesInterval: 1000
-      });
-    }
+const postRequest = async (requestOptions:object) => {
+  try {
+    await fetch(
+      `${BACKEND_URL}/api/routes`, requestOptions)
+      .then(response => {
+        response.json()
+          .then(data => {
+            alert("Post created");
+            console.log(data);
+          });
+      })
   }
-};
-
-const stopTask = async () => {
-  await Location.stopLocationUpdatesAsync(LOCATION_TASK_NAME);
+  catch (error) {
+    console.error(error);
+  }
 }
 
 export default function ModalScreen() {
-  const [backgroundLocaton, setBackgroundLocation] = useState(false);
+  const [trackingEnabled, setTrackingEnabled] = useState(false);
+
+  const startStopBackgroundLocation = async (switchState: boolean) => {
+    if (switchState) {
+      const { status: foregroundStatus } = await Location.requestForegroundPermissionsAsync();
+      if (foregroundStatus === 'granted') {
+        const { status: backgroundStatus } = await Location.requestBackgroundPermissionsAsync();
+        if (backgroundStatus === 'granted') {
+          console.log('Start task')
+          setTrackingEnabled(true);
+          await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+            accuracy: Location.Accuracy.Highest,
+            deferredUpdatesInterval: 1000
+          });
+        }
+      }
+    }
+    else {
+      console.log('Stop task')
+      await Location.stopLocationUpdatesAsync(LOCATION_TASK_NAME);
+      setTrackingEnabled(false);
+    }
+  };
 
   useEffect(() => {
     let TaskStatus = TaskManager.isTaskRegisteredAsync(LOCATION_TASK_NAME);
     TaskStatus.then((value) => {
-      setBackgroundLocation(value)
+      setTrackingEnabled(value)
     });
   }, [])
 
-  console.log(backgroundLocaton);
+  console.log(trackingEnabled);
   return (
     <View style={styles.container}>
       {/* Use a light status bar on iOS to account for the black space above the modal */}
       <StatusBar style={Platform.OS === 'ios' ? 'light' : 'auto'} />
-      <Text style={styles.title}>Modal</Text>
+      <Text style={styles.title}>Enable Tracking</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      {backgroundLocaton == false ? 
-        (<Button onPress={requestPermissions} title="Enable background location" />)
-        : <Button onPress={stopTask} title="Disable background location" />}
+      <Switch
+        trackColor={{ false: '#595959', true: '#05c141' }}
+        ios_backgroundColor="#3e3e3e"
+        thumbColor={'#bac1bc'}
+        onValueChange={startStopBackgroundLocation}
+        value={trackingEnabled}
+      ></Switch>
     </View>
   );
 }
@@ -57,7 +85,14 @@ TaskManager.defineTask(LOCATION_TASK_NAME, ({ data, error }) => {
   if (data) {
     const { locations } = data;
     console.log(locations)
-    // do something with the locations captured in the background
+    let route_id = 1;
+    let requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: {route_id, locations}
+    };
+
+    postRequest(requestOptions);
   }
 });
 
